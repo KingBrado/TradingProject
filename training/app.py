b8c21a418818a6d69e75eba4a6a2c30ca07772bf
KingBrado <berardo.m.manzi@gmail.com>
2024-12-02 22:00:43 +0000

    Add indicators

--- training/app.py
+++ training/app.py
@@ -1,17 +1,20 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
-from utils.constants import ASSETS
 import plotly.graph_objects as go
 
+from utils.constants import ASSETS
+from utils.indicators import ComputeRegistry
 
 st.cache_data
 def load_yf_data(ticker: str) -> pd.DataFrame:
-    data = yf.download(ticker, period='2y', interval='1h')
+    data = yf.download(ticker, period='2y', interval='1d', )
     data = data.droplevel(1, axis=1)
+    data.index = data.index.strftime('%Y-%m-%d')
     return data
 
-selected_asset = st.selectbox(
+
+selected_asset = st.sidebar.selectbox(
     'Asset', ASSETS.keys(), key='assets'
 )
 
@@ -22,6 +25,23 @@ st.text(f"Total TimeStamps: {len(data)}")
 st.table(data.tail(n=10))
 
 st.title("Price")
-fig = go.Figure(data=go.Scatter(x=data.index, y=data['Adj Close']))
+fig = go.Figure(data=[
+    go.Scatter(x=data.index, y=data['Adj Close'], name='Adjusted Close'),
+])
+
+indicators = st.sidebar.multiselect(
+    "Technical Indicator",
+    ["Momentum A", "Momentum B", 'Bollinger Bands'],
+    ["Momentum A"],
+)
+
+for indicator in indicators:
+    n_points = st.sidebar.number_input(indicator, min_value=1, max_value=len(data))
+    indicator_df = ComputeRegistry.compute(indicator, data, int(n_points))
+    fig.add_trace(go.Scatter(x=indicator_df.index, y=indicator_df['Adj Close'], name=indicator))
+    if 'Adj Close_lower' in indicator_df.columns:
+        fig = fig.add_trace(go.Scatter(x=indicator_df.index, y=indicator_df['Adj Close_lower'], name='Lower Band'))
+    if 'Adj Close_upper' in indicator_df.columns:
+        fig = fig.add_trace(go.Scatter(x=indicator_df.index, y=indicator_df['Adj Close_upper'], name='Upper Band'))
 
 st.plotly_chart(fig)
---
+++ training/utils/indicators.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from collections.abc import Callable
+
+
+class ComputeRegistry:
+
+    _computes = {}
+    @classmethod
+    def register(cls, indictor_type: str, compute: Callable):
+        cls._computes[indictor_type] = compute
+
+    @classmethod
+    def compute(cls, indicator_type: str, data: pd.DataFrame, n_points: int) -> pd.DataFrame:
+        return cls._computes[indicator_type](data, n_points)
+
+
+def register_compute(indicator_types: list[str]) -> Callable:
+    def inner(fn: Callable) -> Callable:
+       for indicator in indicator_types:
+            ComputeRegistry.register(indicator, fn)
+       return fn
+    return inner
+
+
+@register_compute(['Momentum A', 'Momentum B'])
+def compute_momentum(data: pd.DataFrame, n_points: int) -> pd.DataFrame:
+    return data.rolling(n_points).mean().dropna()
+
+
+@register_compute(['rolling_std'])
+def compute_rolling_std(data: pd.DataFrame, n_points: int) -> pd.DataFrame:
+    return data.rolling(n_points).std().dropna()
+
+
+@register_compute(['Bollinger Bands'])
+def compute_bollinger_bands(data: pd.DataFrame, n_points: int) -> pd.DataFrame:
+    momentum = compute_momentum(data, n_points)
+    std = compute_rolling_std(data, n_points)
+    lower_band = momentum - 2 * std
+    upper_band = momentum + 2 * std
+    return momentum.join(
+        lower_band.join(upper_band, lsuffix='_lower', rsuffix='_upper'),
+        lsuffix='_momentum'
+    )
+
+    
